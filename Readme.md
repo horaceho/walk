dfcef8d9640d0aad08770eba1adc47e7e8647acc
Horace Ho <horaceh@gmail.com>
2020-12-20 14:46:28 +0800

    initial swiftui app for macos

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## 走吧・Walk
+
+Get up, ... and walk.
+
+## Copyright
+
+&copy; 2020 [horaceho.com](https://horaceho.com)
---
+++ Walk/WalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkApp.swift
+//  Walk
+//
+//  Created by Horace Ho on 2020/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
